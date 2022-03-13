92739554f72f67b2aea2ad998e34b7bdd872ee97
nazonasirkey <abdinasirmaalim23@gmail.com>
2022-03-13 14:09:47 +0300

    add license

---
+++ index.js
@@ -0,0 +1,10 @@
+    function validate() { 
+    var day = document.getElementById("day")
+    var dd = parseInt(day);
+    var month = document.getElementById("month")
+    var mm = parseInt(month);
+    var year = document.getElementById("year")
+    cc = year.substring(0,2);
+    yy = year.substring(2,4);
+
+    }
